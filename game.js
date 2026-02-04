41532318fa57fe130ce63a3e89a61f7755c8c31c
TheKrakken11 <clarkwwhalen@gmail.com>
2026-02-03 19:17:53 -0500

    Log color attributes for geometry in game.js
    Add logging for color attributes in geometry.

--- game.js
+++ game.js
@@ -67,6 +67,11 @@ async function init3d() {
       console.log('  map: ', m.map);
       console.log('  uv: ', !!child.geometry.attributes.uv);
       console.log('  uv2: ', !!child.geometry.attributes.uv2);
+      console.log('  color_0 attribute: ', child.geometry.attributes.color);
+      console.log('  first vertex color: ',
+                  child.geometry.attributes.color.getX(0),
+                  child.geometry.attributes.color.getY(0),
+                  child.geometry.attributes.color.getZ(0)
       console.log('  baseColorTexture: ',
                   m.map ? 'YES' : 'NO'
                   );
