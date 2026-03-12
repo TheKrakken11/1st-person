1d5f63e7925f754a4ea4e3e93898b1c2c66d73fe
TheKrakken11 <clarkwwhalen@gmail.com>
2026-03-11 20:57:41 -0400

    Update game.js

--- game.js
+++ game.js
@@ -122,7 +122,7 @@ async function init3d() {
   scene.add(plane);
   plane.scale.set(0.3048, 0.3048, 0.3048);
   plane.position.y = 1000;
-  plane.quaternion.set(0,0,0,1); // ensure plane starts upright
+  plane.rotation.x = 1 * Math.PI / 180
   thrust.play();
   const camOff = new THREE.Vector3(0, 5, 15);
   plane.add(camera);
